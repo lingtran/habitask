0de03a372ddccb48eccd1161b4bed06a096d3e2c
lingtran <lxiutran@gmail.com>
2016-05-01 17:16:05 -0600

    Rewrite routes to map HTTP verbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  get '/', to: 'home#index', as: :root
 
-  resources :tasks, only: [:index, :show]
-  resources :cart_tasks, only: [:create, :destroy]
-  resources :volunteers, only: [:create, :new, :edit]
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  post '/cart_tasks', to: 'cart_tasks#create', as: :cart_tasks
+  delete '/cart_tasks/:id', to: 'cart_tasks#destroy', as: :cart_task
+
+  get '/volunteers/new', to: 'volunteers#new', as: :new_volunteer
+  post '/volunteers', to: 'volunteers#create', as: :volunteers
+  get '/volunteers/:id/edit', to: 'volunteers#edit', as: :edit_volunteer
 
   get '/dashboard', to: 'volunteers#show'
   get "/cart", to: 'cart_tasks#show'
@@ -21,7 +27,11 @@ Rails.application.routes.draw do
     get '/dashboard', to: 'volunteers#show'
     patch '/dashboard', to: 'volunteers#update'
     get '/edit', to: 'volunteers#edit'
-    resources :tasks, only: [:new, :create, :edit, :update]
   end
 
+  get '/admin/tasks/new', to: 'admin/tasks#new', as: :new_admin_task
+  post '/admin/tasks', to: 'admin/tasks#create', as: :admin_tasks
+  get '/admin/tasks/:id/edit', to: 'admin/tasks#edit', as: :edit_admin_task
+  patch '/admin/tasks/:id', to: 'admin/tasks#update', as: :admin_task
+  put '/admin/tasks/:id', to: 'admin/tasks#update'
 end
